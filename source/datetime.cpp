cde48b0e43b40c05360caaccf64a023b9177f8d2
bernardogiordano <bernardogiordano369@gmail.com>
2017-12-11 16:48:56 +0100

    fix #26, #33

--- source/datetime.cpp
+++ source/datetime.cpp
@@ -36,7 +36,7 @@ std::string getPathDateTime(void)
 	time_t unixTime = time(NULL);
 	struct tm* timeStruct = gmtime((const time_t*)&unixTime);
 	
-	sprintf(buf, "%02i-%02i-%02i_%02i-%02i-%02i", timeStruct->tm_mday, timeStruct->tm_mon + 1, timeStruct->tm_year + 1900, timeStruct->tm_hour, timeStruct->tm_min, timeStruct->tm_sec);
+	sprintf(buf, "%04i%02i%02i-%02i%02i%02i", timeStruct->tm_year + 1900, timeStruct->tm_mon + 1, timeStruct->tm_mday, timeStruct->tm_hour, timeStruct->tm_min, timeStruct->tm_sec);
 	
 	std::string ret(buf);
 	return ret;	
