3b1afe596a798d56a8df574ad40407200e882218
Bernardo Giordano <bernardogiordanp3569@gmail.com>
2018-02-06 13:16:28 +0100

    fix Ultramoon extdata redirection

--- source/title.cpp
+++ source/title.cpp
@@ -291,16 +291,16 @@ u32 Title::getExtdataId(void)
 	u32 low = getLowId();
 	switch(low)
 	{
-		case 0x00055E00: return 0x0000055D; // Pokémon Y
-		case 0x0011C400: return 0x000011C5; // Pokémon Omega Ruby
-		case 0x00175E00: return 0x00001648; // Pokémon Moon
+		case 0x00055E00: return 0x055D; // Pokémon Y
+		case 0x0011C400: return 0x11C5; // Pokémon Omega Ruby
+		case 0x00175E00: return 0x1648; // Pokémon Moon
 		case 0x00179600:
-		case 0x00179800: return 0x00001794; // Fire Emblem Conquest SE NA
+		case 0x00179800: return 0x1794; // Fire Emblem Conquest SE NA
 		case 0x00179700:
-		case 0x0017A800: return 0x00001795; // Fire Emblem Conquest SE EU
+		case 0x0017A800: return 0x1795; // Fire Emblem Conquest SE EU
 		case 0x0012DD00:
-		case 0x0012DE00: return 0x000012DC; // Fire Emblem If JP
-		case 0x001B5100: return 0x001B5000; // Pokémon Ultramoon
+		case 0x0012DE00: return 0x12DC; // Fire Emblem If JP
+		case 0x001B5100: return 0x1B50; // Pokémon Ultramoon
 	}
 	
 	return low >> 8;
