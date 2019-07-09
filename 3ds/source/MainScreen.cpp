727b00edcc8418e666a75ebe21d5ae3c9c1f0e4a
BernardoGiordano <bernardogiordano369@gmail.com>
2019-07-09 22:51:52 +0200

    Shouldn't be issues with #202 anymore

--- 3ds/source/MainScreen.cpp
+++ 3ds/source/MainScreen.cpp
@@ -248,7 +248,15 @@ void MainScreen::updateSelector(void)
 {
     if (!g_bottomScrollEnabled) {
         if (getTitleCount() > 0) {
-            hid.update(getTitleCount());
+            size_t count    = getTitleCount();
+            hid.update(count);
+            // change page
+            if (hidKeysDown() & KEY_L) {
+                hid.pageBack(count);
+            }
+            else if (hidKeysDown() & KEY_R) {
+                hid.pageForward(count);
+            }
             directoryList->resetIndex();
         }
     }
