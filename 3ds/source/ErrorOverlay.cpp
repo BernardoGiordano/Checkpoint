141c2060512fa76e475cf301e9ae32ffac933f95
LiquidFenrir <liquidfenrir@outlook.fr>
2020-06-27 23:09:18 +0200

    a few more copyright date changes

--- 3ds/source/ErrorOverlay.cpp
+++ 3ds/source/ErrorOverlay.cpp
@@ -1,6 +1,6 @@
 /*
  *   This file is part of Checkpoint
- *   Copyright (C) 2017-2019 Bernardo Giordano, FlagBrew
+ *   Copyright (C) 2017-2020 Bernardo Giordano, FlagBrew
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
